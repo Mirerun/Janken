8e98864f438e4883120a1fd6315c9c61f7e55818
mirei <mirei.saxophone1010@gmail.com>
2021-05-12 00:38:28 +0900

    文字の色をColor.rgbで指定

---
+++ app/src/main/java/app/murauchi/mirerun/janken/MainActivity.kt
@@ -0,0 +1,85 @@
+package app.murauchi.mirerun.janken
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gooButton.setOnClickListener {
+            Player.text = "あなたの手はグーです" //あなた=Player
+            val number: Int = Random.nextInt(3) //0~2
+
+            when (number) {
+                0 -> {
+                    cpu.text = "相手の手はグーです" //相手＝cpu
+                    result.text = "引き分けです"
+                    result.setTextColor(Color.rgb(0, 255, 0 ))
+                }
+                1 -> {
+                    cpu.text = "相手の手はチョキです"
+                    result.text = "あなたの勝ちです"
+                    //result.setTextColor(Color.parseColor("f44336"))
+                    result.setTextColor(Color.rgb(255, 0, 0 ))
+                }
+                2 -> {
+                    cpu.text = "相手の手はパーです"
+                    result.text = "あなたの負けです"
+                    result.setTextColor(Color.rgb(0, 0, 255 ))
+                }
+            }
+        }
+
+        chokiButton.setOnClickListener {
+            Player.text = "あなたの手はチョキです"
+            val number: Int = Random.nextInt(3)
+
+            when (number) {
+                0 -> {
+                    cpu.text = "相手の手はグーです"
+                    result.text = "あなたの負けです"
+                    result.setTextColor(Color.rgb(0, 0, 255 ))
+                }
+                1 -> {
+                    cpu.text = "相手の手はチョキです"
+                    result.text = "引き分けです"
+                    result.setTextColor(Color.rgb(0, 255, 0 ))
+                }
+                2 -> {
+                    cpu.text = "相手の手はパーです"
+                    result.text = "あなたの勝ちです"
+                    result.setTextColor(Color.rgb(255, 0, 0 ))
+
+                }
+            }
+        }
+
+        paaButton.setOnClickListener {
+            Player.text = "あなたの手はパーです"
+            val number: Int = Random.nextInt(3)
+
+            when (number) {
+                0 -> {
+                    cpu.text = "相手の手はグーです"
+                    result.text = "あなたの勝ちです"
+                    result.setTextColor(Color.rgb(255, 0, 0 ))
+                }
+                1 -> {
+                    cpu.text = "相手の手はチョキです"
+                    result.text = "あなたの負けです"
+                    result.setTextColor(Color.rgb(0, 0, 255 ))
+                }
+                2 -> {
+                    cpu.text = "相手の手はパーです"
+                    result.text = "引き分けです"
+                    result.setTextColor(Color.rgb(0, 255, 0 ))
+                }
+            }
+        }
+    }
+}
