cdff73457939f5091c5929d3ac219a5875b88233
mirei <mirei.saxophone1010@gmail.com>
2021-05-12 21:18:10 +0900

    電卓アプリのレイアウトのみ

---
+++ app/src/main/java/app/murauchi/mirerun/dentaku/MainActivity.kt
@@ -0,0 +1,13 @@
+package app.murauchi.mirerun.dentaku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //参考コード：https://qiita.com/yanasep/items/503b378c439752f70880
+    }
+}
